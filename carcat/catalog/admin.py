e1a948beb5a881cb16385260b23245b0e8a064b5
Korolev Pavel <korolev.vrn@gmail.com>
2020-03-12 19:10:21 +0300

    add models

--- carcat/catalog/admin.py
+++ carcat/catalog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Car, Color, Producer
 
-# Register your models here.
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Producer)
+class ProducerAdmin(admin.ModelAdmin):
+    pass
+
+
--- carcat/catalog/models.py
+++ carcat/catalog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+TRANSMISSION_CHOICE = [
+    (1, "механика"),
+    (2, "автомат"),
+    (3, "робот"),
+]
+
+class Producer(models.Model):
+    name = models.CharField('Производитель', max_length=150)
+
+class Color(models.Model):
+    name = models.CharField('Производитель', max_length=150)
+
+class Car(models.Model):
+    producer = models.ForeignKey('Producer', on_delete=models.CASCADE, null=True)
+    model_of_car = models.CharField('Модель', max_length=150, null=True)
+    year = models.IntegerField('Год выпуска', null=True)
+    transmission = models.SmallIntegerField('Коробка передач', choices=TRANSMISSION_CHOICE, null=True)
+    color = models.ForeignKey('Color', on_delete=models.SET_NULL, null=True)
